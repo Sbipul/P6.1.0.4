2fac6f7304ea446f8fc2f6d3bd47fd12cf56bcdc
Bipul-Ch-Roy <kumarroy.kb.kb@gmail.com>
2021-08-13 17:17:28 -0600

    updated

--- js/script.js
+++ js/script.js
@@ -1,30 +1,41 @@
 var bSkill = document.getElementById('sk');
 var bExper = document.getElementById('ex');
 var bEdu = document.getElementById('edU');
+var focus = document.getElementById('focus');
 
 var sArea = document.getElementById('ss');
 var exArea = document.getElementById('exx');
 var edArea = document.getElementById('eduu');
+var focusArea = document.getElementById('expert');
 
 
 bSkill.addEventListener('click',()=>{
-	sArea.style.display = 'block';
 	exArea.style.display = 'none';
 	edArea.style.display = 'none';
-
+	focusArea.style.display = 'none';
+	sArea.style.display = 'block';
 
 })
 bExper.addEventListener('click',()=>{
 	sArea.style.display = 'none';
-	exArea.style.display = 'block';
 	edArea.style.display = 'none';
-
+	focusArea.style.display = 'none';
+	exArea.style.display = 'block';
 
 })
 bEdu.addEventListener('click',()=>{
 	sArea.style.display = 'none';
 	exArea.style.display = 'none';
+	focusArea.style.display = 'none';
 	edArea.style.display = 'block';
+
+
+})
+focus.addEventListener('click',()=>{
+	sArea.style.display = 'none';
+	exArea.style.display = 'none';
+	edArea.style.display = 'none';
+	focusArea.style.display = 'block';
 
 
 })
